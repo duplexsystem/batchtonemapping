0fb384247cedf0a0d5e7b2d7d26e188acdf39493
budgidiere <sgidiere@gmail.com>
2018-12-21 21:59:09 -0600

    1.4.2

--- main.py
+++ main.py
@@ -150,7 +150,10 @@ for path, subdirs, files in os.walk(getconfig('path')):
                 did = True
                 number = number + 1
                 os.system ('ffmpeg -i "{}" -vf zscale=transfer=linear,tonemap=tonemap=hable:param=1.0:desat=0:peak=10,zscale=transfer=bt709,format=yuv420p -c:v {} -c:a {} "{}"'.format(currentfile, getoptivcodec(getvcodec(currentfile), getconfig('hq')), getoptiacodec(getacodec(currentfile), getconfig('hq')), filename + "tmp." + fileext))
-                os.rename(currentfile, currentfile + ".bak")
+                if getconfig(deleteorig) == True:
+                    os.remove(currentfile)
+                else:
+                    os.rename(currentfile, currentfile + ".bak")
                 os.rename(filename + "tmp2." + fileext, currentfile)
 if did == True:
     print('Tone Maped {} Files'.format())
